2075173533932ef01de35416fa050e96ef969263
Rajkumar Noel Thoujanba Phanjoubam <mrjamba269@gmail.com>
2025-08-17 23:05:45 +0530

    first commit

---
+++ HGL/VertexArray.cpp
@@ -0,0 +1,51 @@
+#include "include/VertexArray.hpp"
+
+namespace HGL
+{
+    VertexArray::VertexArray()
+    {
+        glGenVertexArrays(1, &m_VAO);
+        glBindVertexArray(m_VAO);
+    }
+
+    VertexArray::VertexArray(VertexArray &&other)
+    {
+        this->m_VAO = other.m_VAO;
+        this->m_enbaleIndicesCount = other.m_enbaleIndicesCount;
+
+        other.m_VAO = 0;
+        other.m_enbaleIndicesCount = 0;
+    }
+
+    VertexArray::~VertexArray()
+    {
+        glDeleteVertexArrays(1, &m_VAO);
+    }
+
+    void VertexArray::Bind(void) const
+    {
+        if(m_VAO == 0)
+        {
+            throw InvalidGLObjectBind("Tried to bind invalid VA");
+            return;
+        }
+        glBindVertexArray(m_VAO);
+    }
+
+    void VertexArray::SetVertexBufferLayout(const VertexBufferLayout& vbl)
+    {
+        this->Bind();
+        for(GLsizei i = 0; i < m_enbaleIndicesCount; ++i)
+        {
+            glDisableVertexAttribArray(i);
+        }
+        m_enbaleIndicesCount = 0;
+
+        const void* offset = 0;
+        for(const auto& vbe : vbl.GetVertexLayout())
+        {
+            glVertexAttribPointer(m_enbaleIndicesCount++, vbe.Count, (GLenum) vbe.Type, vbe.Type == VBLDataTypes::F32 || vbe.Type == VBLDataTypes::F64 ? GL_FALSE : GL_TRUE, vbl.GetStride(), offset);
+            offset = reinterpret_cast<const void*>(reinterpret_cast<size_t>(offset) + vbe.Count * GetVBLTypeSize(vbe.Type));
+        }
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <iterator>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include "HGL/include/Window.hpp"
+#include "HGL/include/VertexBuffer.hpp"
+#include "HGL/include/Shader.hpp"
+#include "HGL/include/IndexBuffer.hpp"
+#include "HGL/include/Texture.hpp"
+#include <print>
+#include <string>
+#include "HGL/include/VertexBufferLayout.hpp"
+#include "HGL/include/VertexArray.hpp"
+
+#include <glm/vec4.hpp>
+
+glm::vec4 vertices[] = 
+{
+    glm::vec4(0.5f, 0.5f, 1.0f, 1.0f), // top right
+    glm::vec4(0.5f, -0.5f, 1.0f, 0.0f), // bottom right
+    glm::vec4(-0.5f, -0.5f, 0.0f, 0.0f), // bottom left
+    glm::vec4(-0.5f, 0.5f, 0.0f, 1.0f) // top left
+};
+uint32_t indices[] = {0, 1, 3, 1, 2, 3};
+int main(void)
+{
+    HGL::WindowProperties windowProperties;
+    HGL::WindowError err;
+    HGL::Window window(windowProperties, err);
+    if(err != HGL::WindowError::NONE)
+    {
+        std::println("window done fuked up");
+    }
+    glClearColor(0.3f, 0.7f , 0.5f, 1.0f);
+    HGL::VertexArray VAO;
+
+    HGL::VertexBufferLayout vbl;
+    vbl.AddElements<float, 2>(); // vertex pos
+    vbl.AddElements<float, 2>();   //tex cords
+    
+    HGL::VertexBuffer vb(vertices, sizeof(vertices), HGL::VertexBuffer::Usage::STATIC_DRAW);
+    vb.Bind();
+
+    VAO.SetVertexBufferLayout(vbl);
+    VAO.SetVertexBufferLayout(vbl);
+
+    HGL::IndexBuffer ib(indices, 6, HGL::IndexBuffer::Usage::STATIC_DRAW);
+    HGL::Shader shader("vertex.glsl", "fragment.glsl");
+    if(shader.GetStatus().ErrorCode != HGL::ShaderError::ERROR_NONE)
+    {
+        std::println("Shader Error: {}", shader.GetStatus().ErrorMessage);
+    }
+
+    shader.Bind();
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (const void* )0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (const void*) (sizeof(float) * 2));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    HGL::Texture2D tex("brick.jpg");
+    tex.Bind(0);
+    shader.SetUniform<int>(std::string_view("u_TexSlot"), 0);
+    while(!window.ShouldClose()) 
+    {
+        double begin = glfwGetTime();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(uint32_t), GL_UNSIGNED_INT, nullptr);
+        window.SwapBuffers();
+        window.PollEvents();
+        double end = glfwGetTime();
+        //std::println("FPS: {}", 1.0 / (end - begin));
+        (void) begin;
+        (void) end;
+    }
+}
